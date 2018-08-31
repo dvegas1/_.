db988aeae6f122292813e5dd982c482d352a0f2d
darwin <darwinvegas1@gmail.com>
2018-08-31 11:39:20 -0400

    ultimo

--- p.php
+++ p.php
@@ -1,8 +1,8 @@
 <?php 
 
 
-//$datos=file_get_contents("php://input",true); 
-$datos='LTE,Android,597 MB,597 MB,100 %,Cargando Via AC'; 
+$datos=file_get_contents("php://input",true); 
+//$datos='LTE,Android,597 MB,597 MB,100 %,Cargando Via AC'; 
 
 echo $datos;
 
